f1e92069c945c900c5d3944451aebb44e72c5425
Sofi <sofikruk0@gmail.com>
2026-03-15 14:03:22 -0300

    Initial Commit

---
+++ WarOfCards/WarOfCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarOfCardsApp.swift
+//  WarOfCards
+//
+//  Created by Софи on 15.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct WarOfCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
